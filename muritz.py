a0790f18cb31666f0b2e5f28a0a202328025462b
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2013-07-23 10:45:46 +1200

    revamped how to call muritz; should now be more straightforward for others

--- muritz.py
+++ muritz.py
@@ -1,24 +1,21 @@
+#!/usr/bin/python
 
+import random
 import subprocess
 import sys
 
 from pymfinder import motif_roles,print_roles
+from option_parser import parse_cl_options
 
 def read_network(filename):
 	network = []
-	inFile = open(netfile1,'r')
+	inFile = open(filename,'r')
 	for line in inFile:
 		network.append(line.strip().split())
 	inFile.close()
 	return network
 
-def muritz_input(netfile1, netfile22):
-	net1 = read_network(netfile1)
-	net2 = read_network(netfile2)
-
-	net1_roles = motif_roles(netfile1,motifsize=3)
-	net2_roles = motif_roles(netfile1,motifsize=3)
-
+def muritz_input(net1, net2, net1_roles, net2_roles):
 	all_roles = net1_roles[net1_roles.keys()[0]].keys()
 
 	input = []
@@ -40,11 +37,37 @@ def muritz_input(netfile1, netfile22):
 
 	return '\n'.join(input)
 
-def muritz(netfile1, netfile2):
-	muritz_in = muritz_input(netfile1, netfile2)
+def muritz(options, args):
+	net1 = read_network(args[0])
+	net2 = read_network(args[1])
+
+	if options.roles1:
+		sys.stderr.write("Sorry, the -r option isn't implemented yet.\n")
+		sys.exit()
+		#net1_roles = read_roles(options.roles1)
+	else:
+		net1_roles = motif_roles(args[0],motifsize=3,)
+
+	if options.roles2:
+		sys.stderr.write("Sorry, the -s option isn't implemented yet.\n")
+		sys.exit()
+		#net2_roles = read_roles(options.roles2)
+	else:
+		net2_roles = motif_roles(args[1],motifsize=3,)
+
+	muritz_in = muritz_input(net1, net2, net1_roles, net2_roles)
+
+	# get a random seed
+	rnd_seed = random.randint(0,sys.maxint)
+
+	# do we want to track the output from muritz?
+	if options.verbose:
+		vflag = "-v"
+	else:
+		vflag = ""
 
 	# call the muritz alignment code
-	command = "./muritz/src/muritz"
+	command = "GSL_RNG_SEED=%s ./muritz/src/muritz %s" % (rnd_seed, vflag)
 	process = subprocess.Popen(command,
     	                       stdin=subprocess.PIPE,
         	                   stdout=subprocess.PIPE,
@@ -52,30 +75,21 @@ def muritz(netfile1, netfile2):
                 	           shell=True)
 
 	muritzout, muritzerr = process.communicate(muritz_in)
-	print(muritzout)
+
+	muritzerr = [i for i in muritzerr.split('\n') if "GSL_RNG_SEED" not in i and i != '']
+	if muritzerr:
+		sys.stderr.write('\n'.join(muritzerr))
+
+	sys.stdout.write(muritzout)
 
 ########################################
 ########################################
 ########################################
 ########################################
 
-netfile1 = "./data/test.net"
-netfile2 = "./data/test2.net"
+def main():
+	options, args = parse_cl_options()
+	muritz(options, args)
 
-muritz(netfile1, netfile2)
-
-
-
-#process.stdin.write("\n".join([' '.join(map(str,[n]+[net2_roles[n][r] for r in all_roles])) for n in net2_roles]))
-#process.stdin.close()#process.stdin.write("/#/##/##/#/\n")
-
-#sys.stdout.write('\n'.join(input))
-
-#process.wait()
-
-
-#for l in process.stdout:
-# 	print l
-
-# print line
-
+if __name__ == "__main__":
+	main()
